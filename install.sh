338948830a730cd7ef1e19d5bb44ff1f80d45efe
hristotanev <hristotanev7@gmail.com>
2022-05-27 00:43:50 +0100

    Fix installer script

--- install.sh
+++ install.sh
@@ -4,9 +4,11 @@ if [[ ! -e $HOME/.dotfiles ]]; then
   git clone https://github.com/hristotanev/.dotfiles.git $HOME/.dotfiles
 fi
 
+cd $HOME/.dotfiles
+
 os=$(uname | awk '{ print tolower($0) }')
 if [[ $os == "linux" || $os == "darwin" ]]; then
-  cp rcm/rcrc.$os rcrc && cp rcrc ~/.rcrc
+  cp ./rcm/rcrc.$os rcrc && cp ./rcrc $HOME/.rcrc
   cd && rcup
 else
   echo "OS not supported."
