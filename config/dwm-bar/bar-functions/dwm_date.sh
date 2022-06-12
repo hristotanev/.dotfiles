3fe91c30f85e4e46e91723e431883a796e810e92
hristotanev <hristotanev7@gmail.com>
2022-06-12 15:47:22 +0100

    Update some config

--- config/dwm-bar/bar-functions/dwm_date.sh
+++ config/dwm-bar/bar-functions/dwm_date.sh
@@ -8,7 +8,7 @@
 dwm_date () {
     printf "%s" "$SEP1"
     if [ "$IDENTIFIER" = "unicode" ]; then
-        printf "📆 %s" "$(date "+%a %d %b %H:%M")"
+        printf "%s" "$(date "+%a %d %b %H:%M")"
     else
         printf "DAT %s" "$(date "+%a %d %b %H:%M")"
     fi
