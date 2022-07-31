3d5b848a2f91af5f9d889a3b06fd2b0fc1b2b5c7
hristotanev <hristotanev7@gmail.com>
2022-07-31 16:08:49 +0100

    General clean up

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -4,10 +4,11 @@ require 'settings'
 require 'remaps'
 
 -- Custom plugin config
-require 'configs.statusline'
-require 'configs.vimtex'
-require 'configs.telescope'
-require 'configs.go'
-require 'configs.clang'
-require 'configs.wilder'
-require 'configs.cmp'
+require 'configs.nvim.statusline'
+require 'configs.nvim.telescope'
+require 'configs.nvim.wilder'
+require 'configs.nvim.cmp'
+
+require 'configs.languages.vimtex'
+require 'configs.languages.go'
+require 'configs.languages.clang'
