e3fb81e5a96bf3a2a4dc022660b091da94829a18
hristotanev <hristotanev7@gmail.com>
2023-01-28 21:57:58 +0000

    feat: Add move.nvim

--- config/nvim/lua/hristotanev/plugins.lua
+++ config/nvim/lua/hristotanev/plugins.lua
@@ -29,6 +29,7 @@ return require('packer').startup(function (use)
   use('nvim-treesitter/nvim-treesitter')
 
   -- Misc
+  use('fedepujol/move.nvim')
   use('vimwiki/vimwiki')
   use('mbbill/undotree')
   use('tpope/vim-commentary')
